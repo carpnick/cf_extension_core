986865150293cf9afb2d045113c7b0c3ebfb2543
Nick Carpenter <nick.carpenter@dotmatics.com>
2023-02-03 11:37:43 -0500

    More common code and better logging

--- example_projects/read_only_resource/tests/unit/__init__.py
+++ example_projects/read_only_resource/tests/unit/__init__.py
@@ -1,5 +1,5 @@
 import os
 import sys
 
-# This makes it so dotmatics_sso_group_info can be imported properly in test code
+# This makes it so the package can be imported properly in test code
 sys.path.append(os.getcwd() + os.sep + "src")
